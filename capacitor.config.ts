1ca058eab61ca4e9fd82b89b2d483526590640c9
ctran <nartc@ctrans-MacBook-Pro.local>
2024-07-08 21:15:08 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ngt-ionic-eats',
+  webDir: 'www'
+};
+
+export default config;
